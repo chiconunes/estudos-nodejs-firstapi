ab4d30f08c4f46b466e7fe38176078cb40593f03
chiconunes <fcn_jr@hotmail.com>
2022-03-10 21:24:28 -0300

    gravando em arquivo json

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 //const { response } = require("express");
 const express = require("express");
 const{randomUUID}= require("crypto");
+const fs = require("fs");
+const { json } = require("express/lib/response");
 
 const app = express();
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json","utf-8",(err,data) => {
+    if (err){
+        console.log(err);
+    }else{
+        products = JSON.parse(data);
+    } 
+});
+
+//-- vamos adicionar novo conteudo
+function ProductFile(){
+    fs.writeFile("products.json",JSON.stringify(products),(err) => {
+        if (err){
+            console.log(err);
+        }else{
+            console.log("Produto inserido");
+        }
+    });
+};
+
 
 app.post("/products",(request,response) => {
     // Nome e preço
@@ -19,6 +41,10 @@ app.post("/products",(request,response) => {
     };
 
     products.push(product);
+
+    // vamos gravar en arquivo    
+    ProductFile();
+
     console.log(products);
     return response.json(product);
 });
@@ -39,12 +65,45 @@ app.get("/products/:id",(request,response) => {
     return response.json(product);
 });
 
-
 app.get("/primeira-rota", (request, response) => {
     return response.send("Acessou a primeira rota com nodemon");
     return response.json({
         message: "Acessou a primeira rota",
     });
+});
+
+app.put("/products/:id",(request,response) => {
+    // Nome e preço
+    const { id}  = request.params;
+    const { name, price } = request.body;
+    
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products[productIndex] = {
+        id:products[productIndex].id,////...products[productIndex],
+        name,
+        price
+    }
+
+     // vamos gravar en arquivo    
+     ProductFile();
+
+    return response.json({message:"Produto alterado com sucesso"});
+});
+
+app.delete("/products/:id",(request,response) => {
+    // Nome e preço
+    const { id}  = request.params;
+    const { name, price } = request.body;
+    
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex,1);
+
+    // vamos gravar en arquivo    
+    ProductFile();
+
+    return response.json({message:"Produto removido com sucesso"});
 });
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
